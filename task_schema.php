04fb3d4258e13dd279d1fdec6dd0f604ee1a34af
cagabi <cagabi@lapiluka.org>
2017-09-10 19:39:04 +0100

    tag added to schema, espiry_date removed

--- task_schema.php
+++ task_schema.php
@@ -5,8 +5,8 @@ $schema['tasks'] = array(
     'userid' => array('type' => 'int(11)', 'Null' => 'NO'),
     'name' => array('type' => 'text'),
     'description' => array('type' => 'text', 'default' => ''),
+    'tag' => array('type' => 'text'),
     'run_on' => array('type' => 'int(10)', 'default' => '0'),
-    'expiry_date' => array('type' => 'int(10)', 'default' => '0'),
     'frequency' => array('type' => 'int(10)', 'default' => '0'),
     'processList' => array('type' => 'text'),
     'enabled' => array('type' => 'tinyint', 'default' => 0)
