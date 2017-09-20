fd556586b1f9eb7ebebffecb2552ee04b3c50d39
cagabi <cagabi@lapiluka.org>
2017-09-20 14:26:42 +0100

    Menu item under Setup

--- task_menu.php
+++ task_menu.php
@@ -1,7 +1,7 @@
 <?php
 
-    $domain = "messages";
-    bindtextdomain($domain, "Modules/rules/locale");
-    bind_textdomain_codeset($domain, 'UTF-8');
+$domain = "messages";
+bindtextdomain($domain, "Modules/rules/locale");
+bind_textdomain_codeset($domain, 'UTF-8');
 
-    $menu_left[] = array('name'=> dgettext($domain, "Tasks"), 'path'=>"task" ,'icon'=>'icon-list', 'session'=>"write", 'order' => 1500 );
+$menu_dropdown_config[] = array('name' => dgettext($domain, "Tasks"), 'path' => "task", 'icon' => 'icon-list', 'session' => "write", 'order' => 1500);
